181e243d3ebd6d2efeb440c22c76fc756086eece
TotalTechGeek <TotalTechGeek@users.noreply.github.com>
2018-10-08 20:46:00 -0500

    Change the output file to be in alphabetical order.

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const bannedLetters = /[gkmqvwxz]/
 // read in the words (\s+ refers to new-line characters)
 let words = fs.readFileSync('words_alpha.txt').toString().split(/\s+/)
 
-// filter out the banned letters, and then sort by length in descending order
+// filter out the banned letters, and then sort by length in descending order (this will allow you to print out the top 10 longest words and such)
 let allowedWords = words.filter(word=>!bannedLetters.test(word)).sort((a,b) => b.length - a.length)
 
 // output the longest word.
 console.log(`Longest Word: ${allowedWords[0]}`)
 
-// export all the words into a text file
-fs.writeFileSync('words_out.txt', allowedWords.join('\r\n'))
+// export all the words into a text file, sorted
+fs.writeFileSync('words_out.txt', allowedWords.sort().join('\r\n'))
